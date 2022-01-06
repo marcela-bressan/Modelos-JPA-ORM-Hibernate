bc125a94ceac765e058d3eef8278116af8935492
MarcelaBressan94 <92112374+MarcelaBressan94@users.noreply.github.com>
2022-01-05 22:37:27 -0300

    Initial commit

---
+++ src/main/java/modelo/basico/NovoUsuario.java
@@ -0,0 +1,46 @@
+package modelo.basico;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class NovoUsuario {
+	public static void main(String[] args) {
+		//Entity Manager -> CRUD 
+		// Entity Manager Factory - > cria Entity Manager
+		//JPQL
+		
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("exercicios-JPA");
+		EntityManager em = emf.createEntityManager();
+		
+		Usuario novoUsuario = new Usuario("Carlos","carlos@local.com.br");
+		Usuario novoUsuario2 = new Usuario("André","andre@local.com.br");
+		Usuario novoUsuario3 = new Usuario("Luana","luana@local.com.br");
+		Usuario novoUsuario4 = new Usuario("Carla","carla@local.com.br");
+		Usuario novoUsuario5 = new Usuario("Guilherme","gui@local.com.br");
+		Usuario novoUsuario6 = new Usuario("Luciano","luciano@local.com.br");
+				
+		
+		//Iniciar
+		em.getTransaction().begin();
+		
+		//inserir o objeto
+		
+		em.persist(novoUsuario);
+		em.persist(novoUsuario2);
+		em.persist(novoUsuario3);
+		em.persist(novoUsuario4);
+		em.persist(novoUsuario5);
+		em.persist(novoUsuario6);
+		
+		//Gravar
+		em.getTransaction().commit();
+		
+		//Fechar
+		em.close();
+		emf.close();
+		
+		
+	}
+}
---
+++ src/main/java/modelo/basico/Usuario.java
@@ -0,0 +1,68 @@
+package modelo.basico;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+	//JPA - ORM - HIBERNATE
+	
+	
+	@Id
+	//auto-increment
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	private String email;
+	
+	//Transient (Quando não for usar no BD)
+	
+	Usuario(){
+		
+	}
+
+
+	public Usuario(String nome, String email) {
+		super();
+		this.nome = nome;
+		this.email = email;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+}
